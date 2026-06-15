da1180dbb7a934d759dc58472bd65e830a21941b
luke <luke.mbindyo@outlook.com>
2026-06-15 16:33:27 -0400

    generated html and added styles to html

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const search = document.querySelector('.anime-search');
 const button = document.querySelector('button');
+const animeGrid = document.querySelector('.anime-grid');
 
 button.addEventListener('click', () => {
     const input = search.value;
@@ -12,19 +13,40 @@ async function GenerateAnimeHTML(animeName) {
 
   const animeData = await response.json();
 
+  console.log(animeData);
+
   let animeHtml = ``;
 
   animeData.data.forEach((anime) => {
     animeHtml += `
     <div class="anime-card">
+    <div class=rating-info>
+    <p>⭐️</p>
+    <p class="rating">${anime.score}</p>
+    </div>
+    <img class="anime-img" src="${anime.images.jpg.image_url}">
 
 
-    <div>
+  
+    <div class="details">
+    <p class="details-p">${anime.type}</p>
+     <p class="details-p">${anime.year}</p>
+     </div>
+
+
+     <div class="title">
+       <p class="anime-title">${anime.title}</p>
+     </div>
     
     
-    
-    
-    `
+
+
+
+    </div> `
+
+
+
   })
 
+    animeGrid.innerHTML = animeHtml
 }
